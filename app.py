0e559e9707d63241a2d7233b95c654ee5961cabe
Wioleta Wrobel <wioletawrobel@MacBook-Air-Wioleta.local>
2025-04-29 14:21:34 +0200

    initialization of API

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/scrape', methods=['GET'])
+
+def scrape():
+    url = request.args.get('url') #url 
+    tag = request.args.get ('div') #div?
+    class_name = request.args.get('element')#class name to download
+
+
+    try:
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, 'html.parser')
+        if class_name:
+            elements = soup.find_all(tag, class_=class_name)
+        else:
+            elements = soup.find_all(tag)
+        texts = [el.get_text(strip=True) for el in elements]
+        return jsonify({'status': 'ok', 'data': texts})
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
